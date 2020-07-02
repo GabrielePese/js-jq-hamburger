da82afb3191e4bb22948220f2c55bc6112720a52
Gabriele <gabrielepese@gmail.com>
2020-07-02 16:53:32 +0200

    finito

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(".header-right a").click (function(){
   $(".hamburger-menu").addClass ("active")
 });
+
+$(".hamburger-menu").click (function(){
+  $(".hamburger-menu").removeClass ("active")
+});
